d66e31a1780aa9f40135a844feb0478c9d0576ef
sowluss <sowluss@gmail.com>
2018-11-13 00:32:42 +0200

    finalised to-do-list adjusting .html .css .js

--- main.js
+++ main.js
@@ -8,6 +8,12 @@
  })
 
  function createItem(x) {
- let ourHTML = `<li>${x}<button>Delete</button></li>`;
+   let ourHTML = `<li>${x}<button id="listBtn" onclick='deleteItem(this)'>Delete</button></li>`;
    ourList.insertAdjacentHTML('beforeend', ourHTML)
- }    
+   //  delete input after submission
+   field.value = '';
+ }
+
+ function deleteItem(deleteElement) {
+    deleteElement.parentElement.remove();
+ }
